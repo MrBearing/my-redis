8ad81b289e9946c260e6aef98382e197fa329b77
Takumi Okamoto <takumi1988okamoto@gmail.com>
2021-05-10 21:36:27 +0900

    update

--- io/src/main.rs
+++ io/src/main.rs
@@ -1,15 +1,27 @@
-use tokio::io;
-use tokio::net::TcpListener;
+use tokio::io::{self, AsyncReadExt,AysyncWriteExt};
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut listener = TcpListener::bind("127.0.0.1:6142").await.unwrap();
+    let socket = TcpStream::connect("127.0.0.1:6142").await?;
 
-    loop {
-        let (mut socket, _) = listener.accept().await?;
+    let (mut reader, mut writer) = io::split(socket);
 
-        tokio::spawn(async move {
-            // ここでデータをコピーする
-        });
+    let  write_task = tokio::spawn(async move {
+        writer.write_all(b"hello\r\n").await?;
+        writer.write_all(b"world\r\n").await?;
+
+        Ok::<(_, io::Error)>(())
+    })
+
+    let mut buf = vec![0; 128];
+
+    loop{
+        let n = reader.read(&mut buf).await?;
+        if n == 0 { break;}
+
+        println!("GOT{:?}",&buf[..n]);
     }
+
+    Ok(())
 }
